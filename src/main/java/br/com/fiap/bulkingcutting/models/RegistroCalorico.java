1f1ef5cfd073f94005a4de05e47becc9452f8a00
FilipeJava <phill.java@gmail.com>
2023-04-19 10:58:28 -0300

    Atualizando imports

--- src/main/java/br/com/fiap/bulkingcutting/models/RegistroCalorico.java
+++ src/main/java/br/com/fiap/bulkingcutting/models/RegistroCalorico.java
@@ -3,7 +3,6 @@ package br.com.fiap.bulkingcutting.models;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
